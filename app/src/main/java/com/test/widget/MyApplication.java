7bbb6a6166475b1761d21684cda6a50e74c8ef4c
spidulica <andrei19081992@gamil.com>
2017-03-26 19:14:46 +0300

    primnul

---
+++ app/src/main/java/com/test/widget/MyApplication.java
@@ -0,0 +1,16 @@
+package com.test.widget;
+
+import android.app.Application;
+
+/**
+ * Created by Spidulica on 22-Mar-17.
+ */
+
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
